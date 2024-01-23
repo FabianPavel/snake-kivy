82cd5f2517c93fea12fb99b56b2a68ad4bf2223c
Pavel <pavel.fabian05@gmail.com>
2024-01-23 08:48:34 +0100

    poznámky, text při konci hry

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from kivy.uix.label import Label
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.core.window import Window
@@ -6,36 +7,47 @@ from kivy.vector import Vector
 from kivy.properties import ObjectProperty, NumericProperty, StringProperty
 from random import randint
 
+# Velikost okna hry
 WINDOW_HEIGHT = 500
 WINDOW_WIDTH = 500
 
+# Velikost hráče a rychlost hry
 PLAYER_SIZE = 20
 GAME_SPEED = .1
 
 
 class Fruit(Widget):
+    """Třída představující ovoce, které se objevuje na hrací ploše."""
+
     def move(self, new_pos):
         self.pos = new_pos
 
 
 class SnakeTail(Widget):
+    """Třída představující článek ocasu hada."""
+
     def move(self, new_pos):
         self.pos = new_pos
 
 
 class SnakeHead(Widget):
+    """Třída představující hlavu hada."""
     orientation = (PLAYER_SIZE, 0)
 
     def reset_pos(self):
+        """Nastaví pozici hlavy hada na střed hrací plochy."""
         self.pos = [int(WINDOW_WIDTH / 2 - (WINDOW_WIDTH / 2 % PLAYER_SIZE)),
                     int(WINDOW_HEIGHT / 2 - (WINDOW_HEIGHT / 2 % PLAYER_SIZE))]
         self.orientation = (PLAYER_SIZE, 0)
 
     def move(self):
+        """Přesune hlavu hada na novou pozici podle aktuální orientace."""
         self.pos = Vector(*self.orientation) + self.pos
 
 
 class SmartGrid:
+    """Třída reprezentující chytrou mřížku pro sledování obsazení pozic na hrací ploše."""
+
     def __init__(self):
         self.grid = [[False for _ in range(WINDOW_HEIGHT)] for _ in range(WINDOW_WIDTH)]
 
@@ -47,6 +59,7 @@ class SmartGrid:
 
 
 class SnakeGame(Widget):
+    """Třída představující samotnou hru."""
     head = ObjectProperty(None)
     fruit = ObjectProperty(None)
     score = NumericProperty(0)
@@ -56,25 +69,38 @@ class SnakeGame(Widget):
     def __init__(self):
         super(SnakeGame, self).__init__()
 
+        # Nastavení velikosti okna
         Window.size = (WINDOW_WIDTH, WINDOW_HEIGHT)
         Window.bind(on_key_down=self.key_action)
 
+        # Kontrola platnosti velikosti hráče
         if PLAYER_SIZE < 3:
-            raise ValueError("Player size should be at least 3 px")
+            raise ValueError("Velikost hráče musí být alespoň 3 px.")
 
+        # Kontrola platnosti velikosti okna ve srovnání s hráčem
         if WINDOW_HEIGHT < 3 * PLAYER_SIZE or WINDOW_WIDTH < 3 * PLAYER_SIZE:
-            raise ValueError("Window size must be at least 3 times larger than player size")
+            raise ValueError("Velikost okna musí být alespoň 3x větší než velikost hráče.")
 
+        # Inicializace hodinového intervalu pro pravidelné obnovení hry
         self.timer = Clock.schedule_interval(self.refresh, GAME_SPEED)
+
+        # Inicializace ocasu a labelu pro zobrazení textu "Game over!"
         self.tail = []
+        self.game_over_label = Label(text="", font_size=20, pos=(WINDOW_WIDTH / 2 - 50, WINDOW_HEIGHT / 2))
+        self.add_widget(self.game_over_label)
+
+        # Restart hry
         self.restart_game()
 
-    def restart_game(self):
+    def restart_game(self, dt=None):
+        """Restartuje hru a nastaví počáteční stav."""
         self.occupied = SmartGrid()
         self.timer.cancel()
         self.timer = Clock.schedule_interval(self.refresh, GAME_SPEED)
         self.head.reset_pos()
         self.score = 0
+        self.game_over_label.text = ""  # Resetuje text na labelu
+        self.game_over = ""  # Resetuje zprávu o konci hry
 
         for block in self.tail:
             self.remove_widget(block)
@@ -90,13 +116,20 @@ class SnakeGame(Widget):
 
         self.spawn_fruit()
 
+    def show_game_over_message(self):
+        """Zobrazí text "Game over!" na střed obrazovky."""
+        self.game_over_label.text = "Konec hry! Stiskněte 'r' pro restart."
+
     def refresh(self, dt):
+        """Obnovuje stav hry v pravidelných intervalech."""
         if not (0 <= self.head.pos[0] < WINDOW_WIDTH) or not (0 <= self.head.pos[1] < WINDOW_HEIGHT):
-            self.restart_game()
+            self.show_game_over_message()
+            self.timer.cancel()
             return
 
         if self.occupied[self.head.pos] is True:
-            self.restart_game()
+            self.show_game_over_message()
+            self.timer.cancel()
             return
 
         self.occupied[self.tail[-1].pos] = False
@@ -117,6 +150,7 @@ class SnakeGame(Widget):
             self.spawn_fruit()
 
     def spawn_fruit(self):
+        """Umístí nové ovoce na náhodnou neobsazenou pozici."""
         roll = self.fruit.pos
         found = False
         while not found:
@@ -131,8 +165,10 @@ class SnakeGame(Widget):
         self.fruit.move(roll)
 
     def key_action(self, *args):
+        """Zpracovává stisknuté klávesy a provádí odpovídající akce."""
         command = list(args)[3]
 
+        #pohyb hada na klávesách w a s d a restart hry na klávese r
         if command == 'w':
             self.head.orientation = (0, PLAYER_SIZE)
         elif command == 's':
